d4c9730bd94efa0851508fcd84fb2a90d983be1f
mengjuL991 <jaydenliaomengju@gmail.com>
2022-11-09 15:40:29 -0700

    Update server.py

--- server.py
+++ server.py
@@ -74,6 +74,7 @@ def flask_post_json():
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
+    
     return redirect("./static/index.html")
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
@@ -82,22 +83,27 @@ def update(entity):
     
     for key, value in flask_post_json().items():
         myWorld.update(entity, key, value)
+        
     return myWorld.get(entity)
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
+    
     return myWorld.world()
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
+    
     return myWorld.get(entity)
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
+    
     myWorld.clear()
+    
     return myWorld.world()
 
 if __name__ == "__main__":
